e5b141c42b8ce2cb5f79c80521d1e96facf79339
Rustam-A-A <aktuganov@yandex.ru>
2023-01-26 18:02:56 +0100

    initial commit

---
+++ src/main/java/com/example/quadratcalc/QuadratCalcApplication.java
@@ -0,0 +1,13 @@
+package com.example.quadratcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadratCalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuadratCalcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quadratcalc/QuadratCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quadratcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuadratCalcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
